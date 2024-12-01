a4e8176f2fa11e3498b598dbd1c4b3223943992d
mad00ness <mad00ness@gmail.com>
2024-12-01 11:49:08 +0300

    uart preamble debug 3

--- components/soyo_meter/soyo_meter.cpp
+++ components/soyo_meter/soyo_meter.cpp
@@ -65,7 +65,7 @@ namespace esphome
                 if (soyo_meter_preamble_check(response))
                 {
                     ESP_LOGW(TAG, "Invalid preamble from SOYO Meter!");
-					ESP_LOGW(TAG, "SOYO Meter response:0x%02X 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X,
+					ESP_LOGW(TAG, "SOYO Meter response:0x%02X 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X",
 					response[0], response[1], response[2], response[3], response[4], response[5], response[6], response[7]);
                     this->status_set_warning();
                     init_uart();
